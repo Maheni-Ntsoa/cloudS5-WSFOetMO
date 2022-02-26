f0637d8917231c3c0f0d186bdb367e7d681b4ac5
unknown <80214803+Maheni-Ntsoa@users.noreply.github.com>
2022-02-26 17:53:47 +0300

    20220226

--- src/main/java/com/webservice/foetmobile/repository/StatutRepository.java
+++ src/main/java/com/webservice/foetmobile/repository/StatutRepository.java
@@ -3,7 +3,12 @@ package com.webservice.foetmobile.repository;
 import com.webservice.foetmobile.modele.Status;
 import com.webservice.foetmobile.modele.TypeSignalement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface StatutRepository extends JpaRepository<Status, Long> {
 
 }
--- src/main/java/com/webservice/foetmobile/repository/TypeSignalementRepository.java
+++ src/main/java/com/webservice/foetmobile/repository/TypeSignalementRepository.java
@@ -3,6 +3,11 @@ package com.webservice.foetmobile.repository;
 import com.webservice.foetmobile.modele.SignalementComplet;
 import com.webservice.foetmobile.modele.TypeSignalement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface TypeSignalementRepository extends JpaRepository<TypeSignalement, Long> {
 }
